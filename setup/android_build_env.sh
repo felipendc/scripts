2b76ad4e0135232f43cc1a31e5d469d47f9592d1
Vicyos <felipendc10phoenix@gmail.com>
2021-01-19 12:56:26 -0300

    add java8

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -29,7 +29,7 @@ fi
 
 sudo DEBIAN_FRONTEND=noninteractive \
     apt install \
-    adb autoconf automake axel bc bison build-essential \
+    openjdk-8-jdk adb autoconf automake axel bc bison build-essential \
     ccache clang cmake expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git gnupg gperf \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
